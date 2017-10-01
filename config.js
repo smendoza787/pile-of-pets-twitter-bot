b6e61eac2cd56e31a2ddb26baf914d1ecaafc186
Sergio Mendoza <smendoza787@gmail.com>
2017-10-01 02:33:13 -0700

    remove config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'IAkLeqQeF7Ff1AOeGDioBvoH2',  
-  consumer_secret: 'BnUDKFcGWUBld9BktvAqKXgovykZ2eSzauJNEUDJaTbkjXek69',
-  access_token: '914376712191963136-XPBQwQIGNn3qxT6haAtp49bwHKFq3c0',  
-  access_token_secret: 'PvTvPQfHVR5dNLcLVY20wyYWROl1WuNJj1qselCERWLda'
-}
